f5a7de68d958d355657ca62cf8b2a312abbed9aa
Alex Suvorov <sahchos93@gmail.com>
2020-06-22 20:10:35 +0300

    Add iterm theme file for iterm, fix setup script

--- setup/setup_new_mac_machine.sh
+++ setup/setup_new_mac_machine.sh
@@ -18,16 +18,8 @@ fi
 # Update homebrew recipes
 brew update
 brew install zsh
-brew install bash # Install latest version of Bash.
 brew install shellcheck  # Linter for shell scripts
-# Configure the new version to be default
-# Source: https://github.com/mathiasbynens/dotfiles/issues/544#issuecomment-104935642
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-# This requires password and won't work on Travis CI
-# Source: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
-if test ! ${CI}; then
-    chsh -s /usr/local/bin/bash
-fi
+chsh -s $(which zsh)
 brew install htop
 
 # Install pyenv
@@ -40,16 +32,14 @@ brew install docker
 # https://stackoverflow.com/questions/19215590/why-cant-i-install-any-gems-on-my-mac
 brew tap raggi/ale && brew install openssl-osx-ca
 # Useful OS X softwares.
-brew cask install google-chrome 
 # Great tool but the cask has been deleted - https://github.com/JadenGeller/Helium/issues/207
 # brew cask install jadengeller-helium  # Web browser on top of all other windows
 brew cask install iterm2
 
 # Install fonts.
-brew tap caskroom/fonts
+brew tap homebrew/cask-fonts
 fonts=(
     font-source-code-pro
-    font-roboto
 )
 echo "Installing fonts..."
 brew cask install ${fonts[@]}
